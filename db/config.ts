98308a983c507d25ce650ba26431b55f430edac2
fm-anderson <78233612+fm-anderson@users.noreply.github.com>
2024-06-15 18:38:32 -0400

    feat: db setup

--- db/config.ts
+++ db/config.ts
@@ -1,6 +1,50 @@
-import { defineDb } from 'astro:db';
+import { defineDb, defineTable, column, NOW } from "astro:db";
 
-// https://astro.build/db/config
+const Customers = defineTable({
+  columns: {
+    id: column.text({ primaryKey: true }),
+    name: column.text(),
+    address: column.text(),
+    email: column.text(),
+    phone: column.text(),
+    createdAt: column.date({ default: NOW }),
+    updatedAt: column.date({ default: NOW }),
+  },
+});
+
+const Items = defineTable({
+  columns: {
+    id: column.text({ primaryKey: true }),
+    sku: column.text({ optional: true }),
+    name: column.text(),
+    desc: column.text({ optional: true }),
+    price: column.number(),
+    createdAt: column.date({ default: NOW }),
+    updatedAt: column.date({ default: NOW }),
+  },
+});
+
+const Invoices = defineTable({
+  columns: {
+    id: column.text({ primaryKey: true }),
+    number: column.text(),
+    url: column.text(),
+    customer: column.text(),
+    date: column.date({ default: NOW }),
+    dueDate: column.date({ optional: true }),
+    total: column.number(),
+    invItems: column.json(),
+    createdAt: column.date({ default: NOW }),
+    updatedAt: column.date({ default: NOW }),
+  },
+  foreignKeys: [
+    {
+      columns: ["customer"],
+      references: () => [Customers.columns.id],
+    },
+  ],
+});
+
 export default defineDb({
-  tables: {}
+  tables: { Customers, Items, Invoices },
 });
--- db/seed.ts
+++ db/seed.ts
@@ -1,6 +1,95 @@
-import { db } from 'astro:db';
+import { db, Customers, Items, Invoices } from "astro:db";
 
-// https://astro.build/db/seed
-export default async function seed() {
-	// TODO
+export default async function () {
+  await db.insert(Customers).values([
+    {
+      id: "SEEDLQLjMH4g9SlHdziwrHPzRy4RJqTC",
+      name: "Bastinkie Thomson",
+      address: "372 Main St, Kramson, CA 00048 (Apt 2)",
+      email: "bastinkie.thomson@example.com",
+      phone: "006 400 0306",
+    },
+    {
+      id: "SEED0lGzvC6ZC5wr24HZ74WmJK87agsQ",
+      name: "Mediphira Bailey",
+      address: "15 Indian Hill Rd, Thozzt, CA 00080",
+      email: "mediphira.bailey@example.com",
+      phone: "050 200 2955",
+    },
+  ]);
+
+  await db.insert(Items).values([
+    {
+      id: "SEEDpjvi4sOgxeGP",
+      sku: "TVM001",
+      name: "32 - 55 TV Mount Installation",
+      desc: "",
+      price: 99.0,
+    },
+    {
+      id: "SEEDoAMWHphnMink",
+      sku: "TVM002",
+      name: "56 - 75 TV Mount Installation",
+      desc: "",
+      price: 169.0,
+    },
+    {
+      id: "SEEDrJNmkcYY49YZ",
+      sku: "TVM003",
+      name: "76 - 85 TV Mount Installation",
+      desc: "",
+      price: 239.0,
+    },
+    {
+      id: "SEEDRUvtSnfeM6h5",
+      sku: "CORD01",
+      name: "Cord Masking (Up to 4 ft)",
+      desc: "",
+      price: 49.0,
+    },
+  ]);
+
+  await db.insert(Invoices).values([
+    {
+      id: "SEED6cc5",
+      number: "INV001",
+      url: "https://example.com/invoices/INV001",
+      customer: "SEEDLQLjMH4g9SlHdziwrHPzRy4RJqTC",
+      dueDate: new Date(Date.now() + 10 * 86400000),
+      total: 148.0,
+      invItems: [
+        {
+          id: "SEEDpjvi4sOgxeGP",
+          sku: "TVM001",
+          name: "32 - 55 TV Mount Installation",
+          desc: "",
+          price: 99.0,
+        },
+        {
+          id: "SEEDRUvtSnfeM6h5",
+          sku: "CORD01",
+          name: "Cord Masking (Up to 4 ft)",
+          desc: "",
+          price: 49.0,
+        },
+      ],
+    },
+    {
+      id: "SEEDf748",
+      number: "INV002",
+      url: "https://example.com/invoices/INV002",
+      customer: "SEED0lGzvC6ZC5wr24HZ74WmJK87agsQ",
+      dueDate: new Date(Date.now() + 20 * 86400000),
+      total: 239.0,
+      invItems: [
+        {
+          id: "SEEDrJNmkcYY49YZ",
+          sku: "TVM003",
+          name: "76 - 85 TV Mount Installation",
+          desc: "",
+          price: 239.0,
+        },
+      ],
+    },
+  ]);
 }
